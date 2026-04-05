14d00808fd58ea23111c536923a1e0a9ef80170c
Елизавета <ha.eliza07@gmail.com>
2026-04-05 13:18:43 +0300

    Галерея

--- six/src/App.js
+++ six/src/App.js
@@ -1,9 +1,25 @@
+import { useRef } from "react";
+import ImageGallery from "react-image-gallery";
+import "react-image-gallery/styles/image-gallery.css";
+import data from './data.json';
 import './App.css';
 
 function App() {
+  const collections = data.collections;
+  const galleryRef = useRef(null);
   return (
     <div className="App">
-      
+      <h1>Моя коллекция фотографий</h1>
+      <div className="image-gallery-wrapper">
+        {
+          collections.map((obj, index) => (
+            <div className="collection" key={index}>
+              <h2>{obj.name}</h2>
+              <ImageGallery items={obj.photos} ref={galleryRef}/>
+            </div>
+          ))
+        }
+      </div>
     </div>
   );
 }
